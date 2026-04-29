34a340427b786c427ce890e60b052fff027d74bb
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-04-30 00:05:31 +0530

    new input system character controller

--- Test Scene/Assets/Script/PlayerMovement.cs
+++ Test Scene/Assets/Script/PlayerMovement.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace PrototypeGames
 {
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement : MonoBehaviour 
     {
         //頑張って
         #region References
@@ -13,13 +15,19 @@ namespace PrototypeGames
 
         #region Variables
 
-        
+        [SerializeField] private float _speedMultiplier = 5.0f;
+        private float _horizontalInput;
+        private float _verticalInput;
+        private Vector3 _direction;
 
         #endregion
 
         #region Functions
 
-        
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _direction = context.ReadValue<Vector2>();
+        }
 
         #endregion
 
@@ -27,10 +35,15 @@ namespace PrototypeGames
         
         void Start()
         {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _verticalInput = Input.GetAxis("Vertical");
         }
         
         void Update()
         {
+            _horizontalInput = Input.GetAxis("Horizontal"); 
+            _verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(_direction*(_speedMultiplier*Time.deltaTime));
         }
         #endregion
     }

6d9643ff43b2c548a550104cf473a5cd09e8ebbb
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-05-08 02:20:14 +0530

    Tying to implement rotation

--- Test Scene/Assets/Script/Player Script/ManualPlayerInput.cs
+++ Test Scene/Assets/Script/Player Script/ManualPlayerInput.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Searcher;
 using UnityEngine;
 
 namespace PrototypeGames
@@ -10,6 +11,11 @@ namespace PrototypeGames
         [SerializeField] private float _playermaxSpeed = 5f;
         private float _sprintfactor = 2f;
         private float storePlayerSpeed;
+        [Header("Rotation")] private  float horizontal;
+        private  float vertical;
+        [SerializeField] private float speed = 10f;
+        [SerializeField] private float rotationSmoothing = 10f;
+        // private Vector3 direction = new Vector3(horizontal, vertical, 0f);
 
         #endregion
 
@@ -48,19 +54,44 @@ namespace PrototypeGames
             }
         }
 
+        private void PlayerRotationUpdate()
+        {
+            horizontal = Input.GetAxis("Mouse X")* rotationSmoothing;
+            vertical = Input.GetAxis("Mouse Y") * speed;
+            Debug.Log(horizontal);
+            horizontal = Mathf.Clamp(1, -90f, 90f);
+            
+
+            transform.Rotate(vertical, horizontal, 0f);
+        }
+
         #endregion
+        
+        // float mouseX = Input.GetAxis("Mouse X");
+        // // Debug.Log(mouseX);
+        // horizontal = Input.GetAxis("Horizontal");
+        // vertical = Input.GetAxis("Vertical");
+        // Debug.Log(" Horizontal " + horizontal + " Vertical "+ vertical );
+        // if (direction.magnitude >= 0.1f)
+        // {
+        //     Quaternion targetRotation = Quaternion.LookRotation(direction);
+        //     transform.rotation =
+        //         Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothing * Time.deltaTime);
+        // }
 
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             storePlayerSpeed = _playermaxSpeed;
+            
         }
 
         // Update is called once per frame
         void Update()
         {
             PlayerMovementUpdate();
+            PlayerRotationUpdate();
         }
     }
 }

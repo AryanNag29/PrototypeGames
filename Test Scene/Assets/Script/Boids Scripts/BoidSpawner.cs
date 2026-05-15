3def9439862ad9c98bf2f2aba06ec5103acaa9ed
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-05-15 22:56:21 +0530

    Boid Spawner Update

--- Test Scene/Assets/Script/Boids Scripts/BoidSpawner.cs
+++ Test Scene/Assets/Script/Boids Scripts/BoidSpawner.cs
@@ -4,16 +4,58 @@ namespace PrototypeGames
 {
     public class BoidSpawner : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        #region Enum
+
+        public enum GismozType
         {
-        
+            Never,
+            SelectedOnly,
+            Always
         }
 
-        // Update is called once per frame
-        void Update()
+        #endregion
+
+        #region References
+
+        public Boid prefab;
+
+        #endregion
+
+
+        #region Variables
+
+        public float spawnRadius = 10;
+        public float spawnCount = 10;
+        public Color colour;
+        public float gizmosVisiblity = 0.3f;
+        public GismozType showSpawnRegion;
+
+        #endregion
+
+        #region Functions
+
+        private void OnDrawGizmos()
         {
-        
+            if (showSpawnRegion == GismozType.Always)
+            {
+                DrawGismos();
+            }
         }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (showSpawnRegion == GismozType.SelectedOnly)
+            {
+                DrawGismos();
+            }
+        }
+
+        private void DrawGismos()
+        {
+            Gizmos.color = new Color(colour.r, colour.g, colour.b, gizmosVisiblity);
+            Gizmos.DrawSphere(transform.position, spawnRadius);
+        }
+
+        #endregion
     }
 }

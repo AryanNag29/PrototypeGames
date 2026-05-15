e0d957f29f9c3de67e5b4014142dd1221d57ad23
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-05-15 19:48:09 +0530

    Made camera track script and gliderscript

---
+++ Test Scene/Assets/Script/GliderScripts/CamaraTrack.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace PrototypeGames
+{
+    public class CamaraTrack : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Test Scene/Assets/Script/GliderScripts/GliderScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace PrototypeGames
+{
+    public class GliderScript : MonoBehaviour
+    {
+        
+    }
+}

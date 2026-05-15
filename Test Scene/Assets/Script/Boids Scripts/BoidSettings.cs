4c8d55a058a4d8ea780c24f2c76281c07fb76a08
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-05-15 23:08:31 +0530

    Boid setting script (Scriptable Object)

---
+++ Test Scene/Assets/Script/Boids Scripts/BoidSettings.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace PrototypeGames
+{
+    public class BoidSettings : ScriptableObject
+    {
+        #region Variables
+
+        //boid Settings
+        
+
+        #endregion
+    }
+}
